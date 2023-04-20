d6b3d8cd358bdfcc1f43cd3b5402179892bb0276
Senlovan <maiconmattossenlovan@gmail.com>
2023-04-19 21:32:02 -0300

    Exercícios Variaveis

---
+++ main.js
@@ -0,0 +1,45 @@
+//let nome = null
+//let idade = null
+//console.log (typeof nome)
+//console.log (typeof idade)
+
+//Por ser uma variavel nula, não é reconhecido como string ou number
+
+//nome = prompt('Qual o seu Nome?')
+//idade = Number (prompt ('Qual a sua idade?'))
+//console.log (typeof nome)
+//console.log(typeof idade)
+
+// agora com uma variavel atribuida o sistema reconhece como texto.
+
+//console.log('Olá', nome,'Você tem', idade, 'anos!')
+
+//const a = 'Você foi trabalhar hoje?'
+//const b = 'Você está usando roupa branca?'
+//const c = 'Você estuda?'
+
+//const d = 'Sim'
+//const e = 'Não'
+//const f = 'Sim'
+
+//console.log (a,d)
+//console.log (b,e)
+//console.log (c,f)
+
+//let a = 60
+//let b = 25
+//let c = a
+
+//a = b
+//b = c
+//c = a
+
+//console.log('O novo valor de a é:', a)
+//console.log('O novo valor de b é:', b)
+
+let usuarioUm = 23
+let usuarioDois = 20
+let x = usuarioUm + usuarioDois
+let y = usuarioUm * usuarioDois
+console.log ('O primeiro número somado ao segundo número resulta em:', x)
+console.log ('O primeiro número multiplicado ao segundo número resulta em:', y)
